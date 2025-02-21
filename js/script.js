645e144529bf61273a4f05deefb60bc39ce53e3f
manqush <abdu2allah7890@gmail.com>
2025-02-21 21:29:10 +0300

    blog page

--- js/script.js
+++ js/script.js
@@ -21,4 +21,65 @@ for (let i = 0; i < BoxOpin.length; i++) {
     });
 }
 
+// main mobile controller
+
+// close menu on mobile and open
+try {
+
+
+
+    let afterAnimationTime=0.6;
+
+    // open btn link
+    let OpenMenu=document.querySelector('.linkOpenHeader');
+
+    // btn close in menu
+    let colseMenu=document.querySelector('.closeMobileMenu');
+    
+    // class parent menu
+    let ParentMenu=document.querySelector('.MobileLeftFixtMenu');
+
+    // class child menu
+    let ChildMenu=document.querySelector('.menuLeftFix');
+
+    // close btn link
+    colseMenu.addEventListener('click',()=>{
+        // add animation
+         ParentMenu.classList.toggle('animate__fadeOut');
+        ChildMenu.classList.toggle('animate__lightSpeedOutLeft');
+
+        // after complete animation
+        setTimeout(()=>{
+            ParentMenu.classList.toggle('MobileLeftFixtMenuClose');
+            ChildMenu.classList.toggle('closeMobileMenuClose');
+
+            // remove class animation of library
+            ParentMenu.classList.toggle('animate__fadeOut');
+            ChildMenu.classList.toggle('animate__lightSpeedOutLeft');
+            
+        },afterAnimationTime * 1000)
+    });
+
+
+
+
+    OpenMenu.addEventListener('click',()=>{
+
+        ParentMenu.classList.toggle('MobileLeftFixtMenuClose');
+        ChildMenu.classList.toggle('closeMobileMenuClose');
+        // add animation
+         ParentMenu.classList.toggle('animate__fadeIn');
+         ChildMenu.classList.toggle('animate__fadeInLeft');
+
+        // after complete animation
+        setTimeout(()=>{
+            
+            ParentMenu.classList.toggle('animate__fadeIn');
+            ChildMenu.classList.toggle('animate__fadeInLeft');
+            
+        },afterAnimationTime * 1000);
+    });
+} catch (error) {
+    
+}
 
